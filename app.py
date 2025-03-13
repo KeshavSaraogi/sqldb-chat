11d477309f28f540e1aa8dadf41e0ce36b13aaaf
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-03-14 01:45:52 +0530

    db configuration

--- app.py
+++ app.py
@@ -18,3 +18,23 @@ MYSQL="USE_MYSQL"
 radio_opt=["Use SQLLite 3 Database- Student.db","Connect to you MySQL Database"]
 
 selected_opt=st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1:
+    db_uri=MYSQL
+    mysql_host=st.sidebar.text_input("Provide MySQL Host")
+    mysql_user=st.sidebar.text_input("MYSQL User")
+    mysql_password=st.sidebar.text_input("MYSQL password",type="password")
+    mysql_db=st.sidebar.text_input("MySQL database")
+else:
+    db_uri=LOCALDB
+
+api_key=st.sidebar.text_input(label="GRoq API Key",type="password")
+
+if not db_uri:
+    st.info("Please enter the database information and uri")
+
+if not api_key:
+    st.info("Please add the groq api key")
+
+## LLM model
+llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
