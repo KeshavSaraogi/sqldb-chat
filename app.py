f8b8d9a16b05980a8521b7c0e5ced8046e54cb7c
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-03-15 00:08:05 +0530

    completing the app

--- app.py
+++ app.py
@@ -38,3 +38,51 @@ if not api_key:
 
 ## LLM model
 llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
+
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        dbfilepath=(Path(__file__).parent/"student.db").absolute()
+        print(dbfilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro", uri=True)
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))   
+    
+if db_uri==MYSQL:
+    db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else:
+    db=configure_db(db_uri)
+
+## toolkit
+toolkit=SQLDatabaseToolkit(db=db,llm=llm)
+
+agent=create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query=st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback=StreamlitCallbackHandler(st.container())
+        response=agent.run(user_query,callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
+
+        
